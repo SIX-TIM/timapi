a30e604f0653ce7a149cd6053c9ddb3e8c34ed33
David Sommerhalder <david.sommerhalder@six-group.com>
2018-08-21 16:15:53 +0200

    Fix stage

--- stage/assets/js/config.js
+++ stage/assets/js/config.js
@@ -2,4 +2,4 @@
 // - "doc" : no banner
 // - "stage": a red banner will appear with "stage version" on it
 // - "archived" : a blue banner will appear with "archived version" on it
-window.type = "doc"
+window.type = "stage"
