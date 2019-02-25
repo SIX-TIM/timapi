f2ed9c02ae99db109c3be16088c2cd91f5017baf
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-02-25 18:36:31 +0100

    Publish staged doc

--- doc/assets/js/config.js
+++ doc/assets/js/config.js
@@ -1,5 +1,1 @@
-// Type of doc:
-// - "doc" : no banner
-// - "stage": a red banner will appear with "stage version" on it
-// - "archived" : a blue banner will appear with "archived version" on it
 window.type = "doc"
