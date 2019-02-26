8be4f539ffa98ec2af20a0a6deaf4be8a3dfe730
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-02-26 10:36:23 +0100

    Archive doc

---
+++ archive/19-02/doc/c/doc/api_version.js
@@ -0,0 +1,1 @@
+timapi = {}; timapi.api_version = "v3.14.0"; timapi.api_lang = "C";
---
+++ archive/19-02/doc/c/doc/resource__parameter__type_8h.js
@@ -0,0 +1,15 @@
+var resource__parameter__type_8h =
+[
+    [ "ta_e_resource_parameter_type_t", "resource__parameter__type_8h.html#a6f8d563c9e0958e940dab312c3a13eff", null ],
+    [ "ta_e_resource_parameter_type", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fb", [
+      [ "ta_c_rpt_undefined", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fba3fe4cf6f89fde28ced047494f229829a", null ],
+      [ "ta_c_rpt_code_check_type", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fba55b588e95ee792144c265d2109f05e49", null ],
+      [ "ta_c_rpt_code_check_key_id", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fba3b7a5d33b2f2980caffb84a5c52ffd03", null ],
+      [ "ta_c_rpt_code_check_data", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fba944a95dccce98df5b1a87c4ed95abe1e", null ],
+      [ "ta_c_rpt_regular_data_query_type", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fbaa413530ab9111c95f3745e85b3df2ef1", null ],
+      [ "ta_c_rpt_regular_data", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fba14f5935cae2f3ea5e9849ba084df986d", null ],
+      [ "ta_c_rpt_petrol_card_data1", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fbaa7e56aed5c3a8b464b3485a6fd8cc05e", null ],
+      [ "ta_c_rpt_petrol_card_data2", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fbafb7b8c6713d6a3cea383a849f3988b47", null ],
+      [ "ta_c_rpt_petrol_card_data3", "resource__parameter__type_8h.html#a24c84814e0ce6a320ff3bd3826a297fbacf33b6be20714c5e787ba88d5c3bef55", null ]
+    ] ]
+];
