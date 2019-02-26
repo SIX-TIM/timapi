47a370933d14f669e9b413c9760c3b68759910ed
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-02-26 16:49:12 +0100

    Archive for commit: 'Doc Update'

---
+++ archive/19-05/c/doc/protocol__type_8h.js
@@ -0,0 +1,8 @@
+var protocol__type_8h =
+[
+    [ "ta_e_protocol_type_t", "protocol__type_8h.html#a0a8aa09b696700577220ecaafba09e36", null ],
+    [ "ta_e_protocol_type", "protocol__type_8h.html#a46759c8ad5a71d1fb5af520c9f7c3d1f", [
+      [ "ta_c_pt_undefined", "protocol__type_8h.html#a46759c8ad5a71d1fb5af520c9f7c3d1faa55eab080e705bf0e820fa482713b20f", null ],
+      [ "ta_c_pt_sixml", "protocol__type_8h.html#a46759c8ad5a71d1fb5af520c9f7c3d1fa40da6b610f05cdfba09f66b3957987cb", null ]
+    ] ]
+];
---
+++ archive/19-05/c/doc/vas__checkout__information_8h.js
@@ -0,0 +1,7 @@
+var vas__checkout__information_8h =
+[
+    [ "ta_vas_checkout_information_get_loyalty_coupons", "vas__checkout__information_8h.html#a0af017748996aba7db5110b8e19080b9", null ],
+    [ "ta_vas_checkout_information_get_loyalty_informations", "vas__checkout__information_8h.html#a3f71f7b92ae7da2cae76a00f4bf22905", null ],
+    [ "ta_vas_checkout_information_get_provide_basket", "vas__checkout__information_8h.html#abd01c613cda975a9216fb50867242b87", null ],
+    [ "ta_vas_checkout_information_get_vas_information", "vas__checkout__information_8h.html#afa71b6a1d06d419906c20332a1a58083", null ]
+];
