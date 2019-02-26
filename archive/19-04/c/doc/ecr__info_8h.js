6ec5cb6109d98ee8a087dfb781cae9ca63e2e310
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-02-26 13:13:46 +0100

    Archive for commit: 'Doc Update'

---
+++ archive/19-04/c/doc/ecr__info_8h.js
@@ -0,0 +1,20 @@
+var ecr__info_8h =
+[
+    [ "ta_ecr_info_create", "ecr__info_8h.html#a6c1970209c463490683002059bbbd548", null ],
+    [ "ta_ecr_info_get_architecture", "ecr__info_8h.html#a5250b8cf39872d625412fea4d3adbe88", null ],
+    [ "ta_ecr_info_get_integrator_solution", "ecr__info_8h.html#af892bf245bf29e5fe3c18be62272c9b7", null ],
+    [ "ta_ecr_info_get_manufacturer_name", "ecr__info_8h.html#a5867ea31e9a00c7b90470712957a04cd", null ],
+    [ "ta_ecr_info_get_name", "ecr__info_8h.html#a095ae3b385eabc6a8c39ca17f109f2a3", null ],
+    [ "ta_ecr_info_get_remote_ip", "ecr__info_8h.html#a61bd935d4d8a6553c21bcb6c17103401", null ],
+    [ "ta_ecr_info_get_serial_number", "ecr__info_8h.html#a81afe41020bddf9277cdc3fd5ccf571e", null ],
+    [ "ta_ecr_info_get_type", "ecr__info_8h.html#a47c12b49dca2c83379048b9ecf922bc6", null ],
+    [ "ta_ecr_info_get_version", "ecr__info_8h.html#a1bfb6d9bde0c09eeaacdb6b59e859df3", null ],
+    [ "ta_ecr_info_set_architecture", "ecr__info_8h.html#a3582df9c3472170a304ffd92a9105a6d", null ],
+    [ "ta_ecr_info_set_integrator_solution", "ecr__info_8h.html#a17acea7610b7e736ac977872be12e16d", null ],
+    [ "ta_ecr_info_set_manufacturer_name", "ecr__info_8h.html#a2f0302f2ca5d6d8c1fe97cd1684f7848", null ],
+    [ "ta_ecr_info_set_name", "ecr__info_8h.html#ac12e0d0733d8d5f9b02895c16bf52de5", null ],
+    [ "ta_ecr_info_set_remote_ip", "ecr__info_8h.html#af73f0fac22f809478c5642f3ae79f7aa", null ],
+    [ "ta_ecr_info_set_serial_number", "ecr__info_8h.html#ab8d30c2b30fc6a558d80689acb413c65", null ],
+    [ "ta_ecr_info_set_type", "ecr__info_8h.html#a28a6429126de01a74edc9ab326a562d7", null ],
+    [ "ta_ecr_info_set_version", "ecr__info_8h.html#ac327e807f26599e84836d2760eaa3dfb", null ]
+];
---
+++ archive/19-04/java/doc/script.js
@@ -0,0 +1,103 @@
+function show(type) {
+    count = 0;
+    for (var key in methods) {
+        var row = document.getElementById(key);
+        if ((methods[key] &  type) != 0) {
+            row.style.display = '';
+            row.className = (count++ % 2) ? rowColor : altColor;
+        }
+        else
+            row.style.display = 'none';
+    }
+    updateTabs(type);
+}
+
+function updateTabs(type) {
+    for (var value in tabs) {
+        var sNode = document.getElementById(tabs[value][0]);
+        var spanNode = sNode.firstChild;
+        if (value == type) {
+            sNode.className = activeTableTab;
+            spanNode.innerHTML = tabs[value][1];
+        }
+        else {
+            sNode.className = tableTab;
+            spanNode.innerHTML = "<a href=\"javascript:show("+ value + ");\">" + tabs[value][1] + "</a>";
+        }
+    }
+}
+
+function setSdkVersion(sdk_version) {
+    if(sdk_version !== undefined) {
+        // Project name
+        var projectName = document.getElementById("projectname");
+        if(projectName != undefined) {
+            // SDK-num
+            var sdknum = document.createElement("span");
+            sdknum.innerHTML = sdk_version;
+            sdknum.id = "sdknum";
+            projectName.appendChild(sdknum);
+        }
+    }
+}
+
+function injectGuidelink() {
+    var navLists = document.getElementsByClassName("navList");
+
+    if((navLists.length > 0) && (navLists[0].title == "Navigation")) {
+        // use last element of nav-bar (help-doc) to create href with correct folder level
+        var href = navLists[0].lastElementChild.lastElementChild.href.replace("help-doc", "../guide");
+
+        // link to implementation guide
+        var guidelink = document.createElement("a");
+        guidelink.href = href;
+        guidelink.innerHTML = "Implementation Guide";
+        guidelink.id = "guidelink_noframes";
+        guidelink.target = "_top"
+        navLists[0].appendChild(guidelink);
+    }
+}
+
+
+function timVersionInject() {
+    // If no sdk version set, try to load as json from asset folder 
+    if(typeof timsdk === "undefined") {
+        timsdk = {};
+        fetch("../../assets/sdk_version.json")
+            .then(response => response.json(), reason => reason)
+            .then(json => setSdkVersion(json.sdk_version), reason => reason);
+    }
+    else {
+        setSdkVersion(timsdk.sdk_version);
+    }
+
+    // TIM API version
+    var projectbrief = document.getElementById("projectbrief");
+    if (projectbrief != undefined) {
+        if((typeof timapi !== "undefined") && (timapi.api_lang != undefined)) {
+            projectbrief.innerHTML = "TIM API " + timapi.api_lang;
+        }
+
+        if((typeof timapi !== "undefined") && (timapi.api_version != undefined)) {
+            var timnum = document.createElement("span");
+            timnum.innerHTML = timapi.api_version;
+            timnum.id = "timnum";
+            projectbrief.appendChild(timnum);
+        }
+    }
+
+    // Implementation guide
+    var projectalign = document.getElementById("projectalign");
+        if(projectalign != undefined) {
+        var guidelink = document.createElement("a");
+        guidelink.href = "../guide.html";
+        guidelink.innerHTML = "Implementation Guide";
+        guidelink.id = "guidelink";
+        projectalign.appendChild(guidelink);
+    }
+}
+
+window.onload = function(evt) { 
+    timVersionInject();
+    injectGuidelink();
+}
