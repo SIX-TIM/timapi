9b162fffe7114bac3bf66817a88a84610068aa87
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-03-04 11:59:40 +0100

    Archive for commit: 'Doc Update'

---
+++ archive/19-07/c/doc/print__format_8h.js
@@ -0,0 +1,11 @@
+var print__format_8h =
+[
+    [ "ta_e_print_format_t", "print__format_8h.html#a454d38bfe7bcc6fd52187159701d0d02", null ],
+    [ "ta_e_print_format", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6", [
+      [ "ta_c_pfmt_undefined", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6a926e322498b124d2afbe38f7a77aaa03", null ],
+      [ "ta_c_pfmt_no_print", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6a34065c00abc896a9b9fb054f0abe840d", null ],
+      [ "ta_c_pfmt_normal", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6a38b24d83679fc5be17b4044eaf039c08", null ],
+      [ "ta_c_pfmt_on_device", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6ad40c92dc5aed981c46beb60198e818d9", null ],
+      [ "ta_c_pfmt_fields_only", "print__format_8h.html#a44afe747b1718776d62504470fdf46c6a96ed0667c7529b7129b3fa0c1aaba6ce", null ]
+    ] ]
+];
---
+++ archive/19-07/c/doc/transaction__type_8h.js
@@ -0,0 +1,29 @@
+var transaction__type_8h =
+[
+    [ "ta_e_transaction_type_t", "transaction__type_8h.html#ab6ee5e4d83de5d339ec03b569082c238", null ],
+    [ "ta_e_transaction_type", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1", [
+      [ "ta_c_tt_undefined", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a0648dbec562790692a38ae234aa5c576", null ],
+      [ "ta_c_tt_purchase", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1ac9d4e87198ab31d00d1688995088c5de", null ],
+      [ "ta_c_tt_credit", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1afbb5f383e092efa3c659924bee4e389a", null ],
+      [ "ta_c_tt_reversal", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a3b3bd7049ce5aaa0d5af81a34f7fa2b1", null ],
+      [ "ta_c_tt_pre_authorization", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a307dc39614b29dbaf2fa7687408d23cb", null ],
+      [ "ta_c_tt_finalize_purchase", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1ad6bbce99f3cc3eaa2b52adca62be1ae0", null ],
+      [ "ta_c_tt_cash_advance", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a1acdd9c7235c6a9f57ab3f32bacbfffa", null ],
+      [ "ta_c_tt_purchase_forced_acceptance", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1abfa34cd2ac3fea6d45fc80dbdefc25b0", null ],
+      [ "ta_c_tt_purchase_with_cashback", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a47d835e8c0cb8f0e26fa7fef06c64a9a", null ],
+      [ "ta_c_tt_purchase_phone_authorized", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1aade97ade4dae0fbc81799e20c93ab5da", null ],
+      [ "ta_c_tt_purchase_phone_ordered", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a437aea786d8c9f051cbae123fd1fe8cb", null ],
+      [ "ta_c_tt_purchase_mail_ordered", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1ab1d5cc4bc4690f73b44f961890ba4794", null ],
+      [ "ta_c_tt_giro", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a231e45d799c08eec3f00bf4d1eab7c82", null ],
+      [ "ta_c_tt_combined", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a3fb1a9c715388c02105ae391af402484", null ],
+      [ "ta_c_tt_authorize_credit", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1af518598d8f9d3066a53d5c896b2831f2", null ],
+      [ "ta_c_tt_authorize_deposit", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1aaddd7f701b9dffb42bf974d9896dc00b", null ],
+      [ "ta_c_tt_reservation", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a8547aaf1574af98275fe03018d5ec27a", null ],
+      [ "ta_c_tt_adjust_reservation", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a215861c9d1ce62a985b66afb13ae2d06", null ],
+      [ "ta_c_tt_cancel_reservation", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a2917c0ad46e658f9798c1788be7a8279", null ],
+      [ "ta_c_tt_purchase_reservation", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a2d31a1f52982de70b8ae25d9b481e9de", null ],
+      [ "ta_c_tt_purchase_reservation_phone_authorized", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a5febf514c1b3f0d5ac66e9420a32a1ae", null ],
+      [ "ta_c_tt_load_voucher", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a2f8a90960e2bac6291716b01f17647da", null ],
+      [ "ta_c_tt_collect_points", "transaction__type_8h.html#a08f07046806962db06121bebefd306d1a1ba813f22e10637a1b54af6538a44181", null ]
+    ] ]
+];
