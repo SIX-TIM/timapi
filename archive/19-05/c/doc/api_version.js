c73fec56c184ea2425bc7bb724904868d3e367f3
timapibuild <dps-sxeb.ios-build@six-group.com>
2019-07-22 12:27:30 +0200

    Archive for commit: 'Publish staged doc'

---
+++ archive/19-05/c/doc/api_version.js
@@ -0,0 +1,1 @@
+timapi = {}; timapi.api_version = "v3.16.0"; timapi.api_lang = "C";
---
+++ archive/19-05/c/doc/navtreedata.js
@@ -0,0 +1,31 @@
+var NAVTREE =
+[
+   
+    [ "API Documentation", null, [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"activate__response_8h.html",
+"command__request_8h.html#ad9827c673d53c16c47c8f127d6331257",
+"custom__receipt__formatter_8h.html#a07c5b9d0b4ce2c471dd995fbae1716b0",
+"financial__transactions_8h.html#acb14a85df061e531e84d9409a28ead04a8a6dd9f46f12f658a12c5ef4a4920536",
+"pos__entry__mode_8h.html#abcd49dcaa026c564bba0a40e0779c198aa92e81f70ba5224b66816b77c7d2a5ea",
+"resource__id_8h.html#a334bce2189c86641d9e5b8aa2dbceec9ac7b49d46f65ca6e69cec8835ae1a8812",
+"string_8h.html",
+"terminal__settings_8h.html#af50057b39c7196dc7d354d7cafe19b9f",
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74aef47ca605613f94b4cc23a96202ceecb"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
