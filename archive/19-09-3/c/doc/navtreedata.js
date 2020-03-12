f43684e5a78b226a04e652041ad3db50c6bb9c1b
timapibuild <dps-sxeb.ios-build@six-group.com>
2020-03-12 15:56:27 +0100

    Archive for commit: 'Publish staged doc for sdk 20-01'

---
+++ archive/19-09-3/c/doc/navtreedata.js
@@ -0,0 +1,32 @@
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", null, [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"activate__response_8h.html",
+"command__request_8h.html#ad9827c673d53c16c47c8f127d6331257",
+"custom__receipt__formatter_8h.html#a07c5b9d0b4ce2c471dd995fbae1716b0",
+"features_8h.html#a7c24d09e8e202cc141c77de53ee379c6",
+"network__information_8h.html#aa5a79d30b1e1b3a8c19038ce17ad74e9",
+"request__type_8h.html#a38268da75c7795fd3a4f057bc6a7fcd2a48427cd573db64b0b249c4de5623ffc2",
+"result__code_8h.html#aebaf00d1ceef4b827866ca9de0bd3320a7baf49e20f9b2c357f96c3e11dc56166",
+"terminal_8h.html#aa52006b12a61c14bb56254a0d3fd0c47",
+"transaction__information_8h.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
