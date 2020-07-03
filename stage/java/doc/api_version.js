6b393f22751709e192d544dafe66960a50f3e0b1
timapibuild <dps-sxeb.ios-build@six-group.com>
2020-07-03 14:04:56 +0200

    Doc Update for sdk 20-05-1

--- stage/java/doc/api_version.js
+++ stage/java/doc/api_version.js
@@ -1,1 +1,1 @@
-timapi = {}; timapi.api_version = "v3.25.0"; timapi.api_lang = "Java"; timapi.api_id = "java";
+timapi = {}; timapi.api_version = "v3.25.1"; timapi.api_lang = "Java"; timapi.api_id = "java";
