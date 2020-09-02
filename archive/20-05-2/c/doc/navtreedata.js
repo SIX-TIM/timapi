b682151f050b0183f48506afb07f1521a6b84c03
timapibuild <dps-sxeb.ios-build@six-group.com>
2020-09-02 09:14:03 +0200

    Archive for commit: 'Publish staged doc'

---
+++ archive/20-05-2/c/doc/navtreedata.js
@@ -0,0 +1,33 @@
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", null, [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"activate__response_8h.html",
+"brand__mode_8h.html#a0b46da6b8e7529349f12aaf94894c5e5",
+"currency_8h.html#ad375db379bcf5a792280a2735f80f7d9a88b37b13381803cb2b1aea2f11d93fb6",
+"enum__string__maps_2dialog__functions_8h.html",
+"list_8h.html#a5918b9f407b67625e9a90fbf33562911",
+"receipt_8h.html#a8d3aa34a6fd1a06bcd0b9fec54443559",
+"resource__id_8h.html#a334bce2189c86641d9e5b8aa2dbceec9a8cc25cb0bac30395ab85975c72c7a2f6",
+"show__dialog__request_8h.html#a3d571fc206e699056b405b20f55a5be8",
+"terminal__listener_8h.html#a364e6b36d27c1dc56376db79bd312fc0",
+"transaction__request_8h.html#a76f655fbbeee335f74f625a187372012"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ archive/20-05-2/swift/doc/api_version.js
@@ -0,0 +1,1 @@
+timapi = {}; timapi.api_version = "v3.25.0"; timapi.api_lang = "Swift";
