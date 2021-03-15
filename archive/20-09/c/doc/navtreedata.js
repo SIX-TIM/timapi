63ea524b4e15425feea587157573e296ab5394ea
timapibuild <dps-sxeb.ios-build@six-group.com>
2021-03-15 10:31:31 +0100

    Archive for commit: 'Publish staged doc TIM SDK 21-01'

---
+++ archive/20-09/c/doc/navtreedata.js
@@ -0,0 +1,34 @@
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", null, [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"activate__response_8h.html",
+"brand__mode_8h.html#a0b46da6b8e7529349f12aaf94894c5e5",
+"currency_8h.html#ad375db379bcf5a792280a2735f80f7d9a88b37b13381803cb2b1aea2f11d93fb6",
+"display__product__info_8h.html#a7ba94ca3c77dafe25ef2da785ba89f18",
+"hardware__information__response_8h.html#ab95d31fcb87c395fd56616bdfab632a7",
+"pos__entry__mode_8h.html#abcd49dcaa026c564bba0a40e0779c198a141bf6fd743de5b8ff002405918e2912",
+"request__type_8h.html#a38268da75c7795fd3a4f057bc6a7fcd2aa12c99a3e91e309b88c00019f1e0e830",
+"result__code_8h.html#aebaf00d1ceef4b827866ca9de0bd3320a70731b776085785520822ae6b30c55bc",
+"terminal_8h.html#a582129e1fe34296f976d3a7a3a670e27",
+"tim__error_8h.html#ad27f349d0651d4f08bbae5831fa5f23a",
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74a989d0033eab8d2ecaa4b0001fcb31341"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ archive/20-09/c/doc/navtreeindex10.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX10 =
+{
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74a989d0033eab8d2ecaa4b0001fcb31341":[1,1,0,1,61,1,5],
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74aa4e0cd244f084bfc9d31036ea99a0ded":[1,1,0,1,61,1,15],
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74ab187c06c1afda3829dc564d263e9d686":[1,1,0,1,61,1,8],
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74ac608fc2d4566185e0ceac199e75cdd12":[1,1,0,1,61,1,19],
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74ad28522e314f5b0264b976002daecd7b8":[1,1,0,1,61,1,12],
+"vas__information__list_8h.html":[1,1,0,73],
+"vas__information__list_8h.html#a27e4117b83cec8cba1fb96a19bc9fee9":[1,1,0,73,2],
+"vas__information__list_8h.html#a30243a0cf0dbe61a741fb33ae375f62a":[1,1,0,73,0],
+"vas__information__list_8h.html#a5dec1389e22373a23995ca0d43d1f21a":[1,1,0,73,1],
+"vas__result_8h.html":[1,1,0,74],
+"vas__result_8h.html#aa7242f5e9fc52e43875185a937249a74":[1,1,0,74,1],
+"vas__result_8h.html#ae30e3696b68523c27e76bce474778fd0":[1,1,0,74,0]
+};
