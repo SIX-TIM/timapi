c490331e4149e1ff84adc6b5eb7969609da22b2c
timapibuild <dps-sxeb.ios-build@six-group.com>
2024-05-30 09:25:33 +0200

    Archive for commit: 'Publish staged doc for SDK 24-05'

---
+++ archive/24-01/c/doc/navtreedata.js
@@ -0,0 +1,58 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", "files.html", [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files_dup" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"account__type_8h.html",
+"brand__bar__brand_8h.html#a065fb75dccd6ee5e82f29d4cce775549a8606a403d77aa1af1cc78fccda277dd8",
+"currency_8h.html#ad375db379bcf5a792280a2735f80f7d9a571fcdd53010627d3021eeefe53158c8",
+"custom__receipt__formatter_8h.html#aa2ad96ca8f3d24cc69d417eaa70dd2e3a385fcfb19cbdf31f92442cc97ca21995",
+"enum__string__maps_2vas__info__type_8h.html",
+"map_8h.html#a32f0a090b118a2b3dc83d0b4d532562a",
+"receipt__item__type_8h.html#a3d3044dd871a9c7d630dbcf3d9b738bea4ebf3df69b2fcdbd8167824afc50ee0f",
+"resource__id_8h.html#a334bce2189c86641d9e5b8aa2dbceec9a9eaccf75658f395bfc03a21489b4fb3d",
+"security__status_8h.html#a8a31df80a5ee73a72036f7eb944c7feda6be1bedfcf7ba06db41a4d824b127ee8",
+"terminal_8h.html#ac4eb1659867f8fee2f20eb55cbeed13d",
+"total_8h.html#a7c9eb0b4bea0ec94e15edc4044510966",
+"vas__info__type_8h.html#ae8d3a7b42cbd07892a5292b8fb5f8f74a1b6b75688d795d8da217d6fadb8fdc10"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ archive/24-01/js/doc/api_version.js
@@ -0,0 +1,1 @@
+timapi = {}; timapi.api_version = "v4.18.0"; timapi.api_lang = "JavaScript"; timapi.api_id = "js";
