ea5b02bffb96c0bec09e1e8ec7532397d31dbbc2
timapibuild <dps-sxeb.ios-build@six-group.com>
2025-04-15 13:57:17 +0200

    Publish staged doc for SDK release 24-09-2

--- doc/assets/js/sdk_version.js
+++ doc/assets/js/sdk_version.js
@@ -1,1 +1,1 @@
-timsdk = {}; timsdk.sdk_version = "24-09-1";
+timsdk = {}; timsdk.sdk_version = "24-09-2";
--- doc/java/doc/api_version.js
+++ doc/java/doc/api_version.js
@@ -1,1 +1,1 @@
-timapi = {}; timapi.api_version = "v3.40.1"; timapi.api_lang = "Java"; timapi.api_id = "java";
+timapi = {}; timapi.api_version = "v3.40.2"; timapi.api_lang = "Java"; timapi.api_id = "java";
