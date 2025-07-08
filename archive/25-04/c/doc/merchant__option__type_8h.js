d2de24dd74da92970173078143822bef6f95a161
timapibuild <dps-sxeb.ios-build@six-group.com>
2025-07-08 12:51:26 +0200

    Archive for commit: 'Publish staged doc for SDK 25-04 (fix-swift-doc-rc5 )'

---
+++ archive/25-04/c/doc/merchant__option__type_8h.js
@@ -0,0 +1,24 @@
+var merchant__option__type_8h =
+[
+    [ "ta_e_merchant_option_type_t", "merchant__option__type_8h.html#a85baa6c1a5b70326c0ab26fb93c563ee", null ],
+    [ "ta_e_merchant_option_type", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362", [
+      [ "ta_c_mot_undefined", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362ae43f438d3f31eae6f936839db3dbf1bf", null ],
+      [ "ta_c_mot_additional_merchant_data", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362ac3d40943756d4fd307026b06ac5936c9", null ],
+      [ "ta_c_mot_multi_account_index", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a742fd34d2ab476eb080dfdcd9551874f", null ],
+      [ "ta_c_mot_multi_contract_index", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a89d691e2262bc262910413c2389d3e6f", null ],
+      [ "ta_c_mot_merchant_tid", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a8100c542cc9670de1db3acea385ebe53", null ],
+      [ "ta_c_mot_clerk_identifier", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a8d573caf210ba36675c5ee4f537a6f91", null ],
+      [ "ta_c_mot_fuel_dispenser_number", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a17ed3ffa300bcf915754877f5deef460", null ],
+      [ "ta_c_mot_posdnumber", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a770fa5489b1d638a8780896eff4a748d", null ],
+      [ "ta_c_mot_receipt_number", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a0ace3ceeec8b839e2b9185ae6acb5c45", null ],
+      [ "ta_c_mot_shift_number", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a6c181472ceda7a1043af135be9c8973a", null ],
+      [ "ta_c_mot_accounting_department", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a9c80bc712cfc818609157a5a34dc12fb", null ],
+      [ "ta_c_mot_accounting_period", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a6831970e2bcbbf2829b428600536c072", null ],
+      [ "ta_c_mot_device_type", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a28bcde67ab27c511cea9ac9596d92532", null ],
+      [ "ta_c_mot_ktucode", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a3fa9c254a99df02e08fc361654a90757", null ],
+      [ "ta_c_mot_station_code", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a42531098bb97678ac56470d20ba36a40", null ],
+      [ "ta_c_mot_wsnumber", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a7ac0a2c136e71c7878f3adff6a01b51a", null ],
+      [ "ta_c_mot_voucher_id", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362ae9a11a11fec15417a0cc90d6ff72a24c", null ],
+      [ "ta_c_mot_dsp_msg_suppress_flag", "merchant__option__type_8h.html#aea3eaf253bcce054769642d812d46362a83c09cb5a6996cd977747d824afdbfd2", null ]
+    ] ]
+];
---
+++ archive/25-04/c/doc/navtreedata.js
@@ -0,0 +1,58 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", "files.html", [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files_dup" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"account__type_8h.html",
+"brand__bar__brand_8h.html#a065fb75dccd6ee5e82f29d4cce775549a8606a403d77aa1af1cc78fccda277dd8",
+"currency_8h.html#ad375db379bcf5a792280a2735f80f7d9a5619f3373b19a4f33cd2e8cef93023c6",
+"custom__receipt__formatter_8h.html#a883a5ff78a5d2ad805f9f6af32a8d2afadefe33602c4b1cea5831b1ac44ebd931",
+"enum__string__maps_2reconfig__mode_8h.html#a433a79581dd24e51f42acefee3413ed1",
+"loyalty__information_8h.html#a32bce129db1ee0103b467d850a4ee178",
+"reason_8h.html#ab10e533f4ad4f7409b4a8fbdaefbf972aad36d9fe5df80e4f66d56e788defd1e4",
+"resource__id_8h.html#a334bce2189c86641d9e5b8aa2dbceec9a20fba294780639ca8961078478cbc4c2",
+"result__code_8h.html#aebaf00d1ceef4b827866ca9de0bd3320a85626d29838bc0f51cd1f364e3040f4f",
+"terminal_8h.html#a4c10d198a9b19da9f6b7c4fa6c6e4715",
+"terminal__settings_8h.html#abd208ce3e6f33bb1b32a2ce44fcfa53d",
+"transaction__response_8h.html#ab363e53b8837c37b3c158cfad13bd7d9"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
