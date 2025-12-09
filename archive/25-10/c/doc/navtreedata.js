7b94828eb7fab129b792e4f31fb33fc2213c2ce0
timapibuild <dps-sxeb.ios-build@six-group.com>
2025-12-09 11:56:17 +0100

    Archive for commit: 'Publish staged doc (Just testing, this will be reverted)'

---
+++ archive/25-10/c/doc/navtreedata.js
@@ -0,0 +1,58 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+   
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "API Documentation", "files.html", [
+      [ "Quick Reference", "usergroup0.html", [
+        [ "terminal", "terminal_8h.html", null ],
+        [ "terminal_settings", "terminal__settings_8h.html", null ],
+        [ "object", "object_8h.html", null ],
+        [ "string", "string_8h.html", null ],
+        [ "list", "list_8h.html", null ],
+        [ "map", "map_8h.html", null ]
+      ] ],
+      [ "All files", "files.html", "files_dup" ]
+    ] ]
+];
+
+var NAVTREEINDEX =
+[
+"accessibility__option__list_8h.html",
+"brand__bar__brand_8h.html#a065fb75dccd6ee5e82f29d4cce775549a6c329f174f29f48cc015e38f1598df24",
+"currency_8h.html#ad375db379bcf5a792280a2735f80f7d9a45cd79ff38c8c804dc47a5ee60776ae0",
+"custom__receipt__formatter_8h.html#a883a5ff78a5d2ad805f9f6af32a8d2afab5e33f99482c2b66a07614bfca952104",
+"enum__string__maps_2protocol__level_8h.html",
+"loyalty__discount_8h.html#a218859dfaf53dd37c2f74ebf2b98c7cf",
+"protocol__type_8h.html#a0a8aa09b696700577220ecaafba09e36",
+"resource__id_8h.html#a334bce2189c86641d9e5b8aa2dbceec9a033f0cf93139dea4e2b3f552f03b9b49",
+"result__code_8h.html#aebaf00d1ceef4b827866ca9de0bd3320a62bb3ba603e1b276770a902156d769b5",
+"terminal_8h.html#a2452ca5e352debc37c22755d6827fbcd",
+"terminal__settings_8h.html#a850f9b5f9f518df6588a9ef7a625e083",
+"transaction__request_8h.html#aa2baa37d543044aeaeb3b1fd444223de"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ archive/25-10/c/doc/vas__info__list__type_8h.js
@@ -0,0 +1,10 @@
+var vas__info__list__type_8h =
+[
+    [ "ta_e_vas_info_list_type_t", "vas__info__list__type_8h.html#a919afa606a8ccff0efd2fbe9a807b5b4", null ],
+    [ "ta_e_vas_info_list_type", "vas__info__list__type_8h.html#a76f59d6e11912abeedcd4e1b307577b9", [
+      [ "ta_c_vilt_undefined", "vas__info__list__type_8h.html#a76f59d6e11912abeedcd4e1b307577b9a2b6d14ede5adbe7acc235270ec53ab60", null ],
+      [ "ta_c_vilt_wol_match", "vas__info__list__type_8h.html#a76f59d6e11912abeedcd4e1b307577b9a5b19aa66c1a32d5caaa6ab3c7183b7fc", null ],
+      [ "ta_c_vilt_wol_program", "vas__info__list__type_8h.html#a76f59d6e11912abeedcd4e1b307577b9a92893ee6aaed7dc61bd1bbd7be7974aa", null ],
+      [ "ta_c_vilt_twint", "vas__info__list__type_8h.html#a76f59d6e11912abeedcd4e1b307577b9a4fe0dacd996945ee5b9426ff0d3478f4", null ]
+    ] ]
+];
