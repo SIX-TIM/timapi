a19155cb85bf3799c8c639d64ae3770b3652c8a6
timapibuild <dps-sxeb.ios-build@six-group.com>
2025-12-09 11:50:21 +0100

    Archive for commit: 'Publish staged doc (Just testing, this will be reverted)'

---
+++ archive/25-06-2/c/doc/reconfig__parameters_8h.js
@@ -0,0 +1,6 @@
+var reconfig__parameters_8h =
+[
+    [ "ta_reconfig_parameters_create", "reconfig__parameters_8h.html#a181c12bd98acf4c7e8092e9df7d235cc", null ],
+    [ "ta_reconfig_parameters_set_acq_id", "reconfig__parameters_8h.html#aacbb12c65ad403bf59fbed76d6faada2", null ],
+    [ "ta_reconfig_parameters_set_reconfig_mode", "reconfig__parameters_8h.html#ad7d6624abc8c47eecaae3f9c9a944737", null ]
+];
